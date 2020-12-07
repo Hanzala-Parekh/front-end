58e6f45f8d2f7ddbd3325a711271ae67d7f68733
Hanzala-Parekh <61308617+Hanzala-Parekh@users.noreply.github.com>
2020-12-08 02:28:22 +0500

    update

--- src/component/parts/Header.js
+++ src/component/parts/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
+import logo from "../../images/logo.png";
 
 const Header = () => {
   return (
@@ -8,7 +9,7 @@ const Header = () => {
         <div className="container">
           <div className="inner-logo-place">
             <a className="navbar-brand" href="index.html">
-              <img src="images/logo.png" alt="" />
+              <img src={logo} alt="" />
             </a>
           </div>
         </div>
--- src/component/screen/NotFound.js
+++ src/component/screen/NotFound.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Header from "../parts/Header";
 import Footer from "../parts/Footer";
 const NotFound = () => {
